9d0a78c5e19461db8f402c5229152b52aca71a0b
Samuel Skanberg <samuel.skanberg@gmail.com>
2015-07-26 23:08:30 +0200

    Added other parties

--- scraper.py
+++ scraper.py
@@ -3,6 +3,7 @@
 from lxml import html
 import requests
 import csv, codecs, cStringIO
+import sys
 
 class UnicodeWriter:
     """
@@ -33,33 +34,45 @@ class UnicodeWriter:
         for row in rows:
             self.writerow(row)
 
+# For some very weird reason, party can contain utf-8 characters. But last_name can. Weird
+party_pages = {
+    'Socialdemokraterna': 'http://www.riksdagen.se/sv/ledamoter-partier/Socialdemokraterna/Ledamoter/', 
+    'Moderaterna': 'http://www.riksdagen.se/sv/ledamoter-partier/Moderata-samlingspartiet/Ledamoter/', 
+    'Sverigedemokraterna': 'http://www.riksdagen.se/sv/ledamoter-partier/Sverigedemokraterna/Ledamoter/',
+    'Miljopartiet': 'http://www.riksdagen.se/sv/ledamoter-partier/Miljopartiet-de-grona/Ledamoter/',
+    'Centerpartiet': 'http://www.riksdagen.se/sv/ledamoter-partier/Centerpartiet/Ledamoter/',
+    'Vansterpartiet': 'http://www.riksdagen.se/sv/ledamoter-partier/Vansterpartiet/Ledamoter/',
+    'Folkpartiet': 'http://www.riksdagen.se/sv/ledamoter-partier/Folkpartiet/Ledamoter/',
+    'Kristdemokraterna': 'http://www.riksdagen.se/sv/ledamoter-partier/Kristdemokraterna/Ledamoter/',
+}
 
-
-# Socialdemokraterna
-page = requests.get('http://www.riksdagen.se/sv/ledamoter-partier/Socialdemokraterna/Ledamoter/')
-tree = html.fromstring(page.text)
-party = "Socialdemokraterna"
-
-
-names = tree.xpath('//section//dl/dt/a/text()')
+print party_pages
 
 with open('names.csv', 'wb') as csvfile:
     fieldnames = ['first_name', 'last_name', 'email', 'party']
     writer = UnicodeWriter(csvfile)
     writer.writerow(fieldnames)
 
-    for name in names:
-        name = name.strip()
-        print name
-        last_name = name[0:name.find(",")].strip()
-        first_name = name[name.find(",")+2:name.find("(", name.find(",")+1)].strip()
-        print first_name + ","+last_name
-         
-        emails = tree.xpath('//section//dl[dt/a/text()[contains(., "'+name+'")]]/dd/noscript/text()')
-        email = emails[0]
-        before_at = email.find("riksdagen.se")
-        
-        formatted_email = email[0:before_at-4]+"@riksdagen.se"
-        print formatted_email
-        writer.writerow([first_name, last_name, formatted_email, party])
+    for party, party_page in party_pages.iteritems():
+
+        page = requests.get(party_page)
+        tree = html.fromstring(page.text)
+
+        names = tree.xpath('//section//dl/dt/a/text()')
+
+
+        for name in names:
+            name = name.strip()
+            print name
+            last_name = name[0:name.find(",")].strip()
+            first_name = name[name.find(",")+2:name.find("(", name.find(",")+1)].strip()
+            print first_name + ","+last_name
+             
+            emails = tree.xpath('//section//dl[dt/a/text()[contains(., "'+name+'")]]/dd/noscript/text()')
+            email = emails[0]
+            before_at = email.find("riksdagen.se")
+            
+            formatted_email = email[0:before_at-4]+"@riksdagen.se"
+            print formatted_email
+            writer.writerow([first_name, last_name, formatted_email, party])
 
